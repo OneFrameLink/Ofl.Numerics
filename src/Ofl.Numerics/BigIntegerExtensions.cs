72868542b885a67fede57847617e7c2e2c0cc930
casperOne <casperOne@caspershouse.com>
2017-05-21 17:05:42 -0400

    Initial code push.

---
+++ src/Ofl.Numerics/BigIntegerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Ofl.Numerics
+{
+    public static class BigIntegerExtensions
+    {
+        public static BigInteger Unchecked(this BigInteger value, int bytes)
+        {
+            // Validate parameters.
+            if (bytes <= 0) throw new ArgumentOutOfRangeException(nameof(bytes), bytes, $"The { nameof(bytes) } parameter must be a positive value.");
+
+            // SHORTCUT: If value is zero, then just return zero, zero fits in any situation.
+            if (value == 0) return value;
+
+            // Get the byte array for the integer.
+            byte[] byteArray = value.ToByteArray();
+
+            // If the length of the bytes is less than the bits / 8 then
+            // return the big integer.
+            if (byteArray.Length <= bytes) return value;
+
+            // Take the last bytes.
+            return new BigInteger(byteArray.Take(bytes).ToArray());
+        }
+    }
+}
---
+++ test/Ofl.Numerics.Tests/BigIntegerExtensonsTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Numerics;
+using Xunit;
+
+namespace Ofl.Numerics.Tests
+{
+    public class BigIntegerExtensonsTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void Test_Unchecked_PositiveOverflow(int iterations)
+        {
+            // Seed the big int and the int.
+            int intValue = 0;
+            BigInteger bigint = 0;
+
+            // Allow overflow.
+            unchecked
+            {
+                // Loop.
+                for (int index = 0; index < iterations; ++index)
+                {
+                    // Add Max value, and 1.  Overflow each time.
+                    intValue += Int32.MaxValue;
+                    intValue += 2;
+
+                    // Update bigint.
+                    bigint = (bigint + Int32.MaxValue).Unchecked(4);
+                    bigint = (bigint + 2).Unchecked(4);
+
+                    // Are equal.
+                    Assert.Equal(intValue, bigint);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void Test_Unchecked_Negative(int iterations)
+        {
+            // Seed the big int and the int.
+            int intValue = 0;
+            BigInteger bigint = 0;
+
+            // Allow overflow.
+            unchecked
+            {
+                // Loop.
+                for (int index = 0; index < iterations; ++index)
+                {
+                    // Subtract Max value, and 1.  Overflow each time.
+                    intValue -= Int32.MaxValue;
+                    intValue -= 2;
+
+                    // Update bigint.
+                    bigint = (bigint - Int32.MaxValue).Unchecked(4);
+                    bigint = (bigint - 2).Unchecked(4);
+
+                    // Are equal.
+                    Assert.Equal(intValue, bigint);
+                }
+            }
+        }
+    }
+}
